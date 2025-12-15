7565ce9ba413eda271f0b424d904f77cd835ee3c
Elumugam <elumugamelumugam@gmail.com>
2025-12-15 07:46:32 +0700

    upadated changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  trailingSlash: true,
+  basePath: '/My-Portoflio',
+  assetPrefix: '/My-Portoflio',
 };
 
 export default nextConfig;
